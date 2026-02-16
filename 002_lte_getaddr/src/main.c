22221d1bb7c41e4bdd37f698f5d23b6cad3b20c2
Mika Tuupola <tuupola@appelsiini.net>
2026-02-16 17:01:51 +0200

    Resolve a hostname

--- 002_lte_getaddr/src/main.c
+++ 002_lte_getaddr/src/main.c
@@ -1,13 +1,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/net/socket.h>
 #include <modem/lte_lc.h>
 #include <modem/nrf_modem_lib.h>
 
-LOG_MODULE_REGISTER(lte_ping, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(lte_getaddr, LOG_LEVEL_INF);
 
 int main(void)
 {
     int rc;
+    struct zsock_addrinfo *result;
+    struct zsock_addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    char ip_str[NET_IPV4_ADDR_LEN];
+
     LOG_INF("Initializing the modem firmware.");
     rc = nrf_modem_lib_init();
     if (rc < 0) {
@@ -23,6 +31,19 @@ int main(void)
         return rc;
     }
     LOG_INF("Connected to LTE network.");
+
+    LOG_INF("Resolving www.appelsiini.net.");
+    rc = zsock_getaddrinfo("www.appelsiini.net", NULL, &hints, &result);
+    if (rc != 0) {
+        LOG_ERR("getaddrinfo failed: %d", rc);
+        return rc;
+    }
+
+    struct sockaddr_in *sockaddr = (struct sockaddr_in *)result->ai_addr;
+    net_addr_ntop(AF_INET, &sockaddr->sin_addr, ip_str, sizeof(ip_str));
+    LOG_INF("IP: %s", ip_str);
+
+    zsock_freeaddrinfo(result);
 
     while (1) {
         k_sleep(K_SECONDS(1));
