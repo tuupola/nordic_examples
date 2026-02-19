c16f44336a31b7ee22f700dff0e24c049bbf7eff
Mika Tuupola <tuupola@appelsiini.net>
2026-02-19 12:41:00 +0200

    Connect and subscribe to MQTT

--- 005_lte_mqtt/README.md
+++ 005_lte_mqtt/README.md
@@ -5,7 +5,7 @@ Make sure you are in SDK shell to have access to the toolchain.
 ```
 $ nrfutil sdk-manager toolchain launch --ncs-version v3.2.1 --shell
 (v3.2.1) host% source ~/ncs/v3.2.1/zephyr/zephyr-env.sh
-(v3.2.1) host% west build -b thingy91x/nrf9151/ns
+(v3.2.1) host% west build -b thingy91x/nrf9151/ns --pristine
 ```
 
 Flash with `nrfutil`. You do not need to be in SDK shell for this.
@@ -15,16 +15,36 @@ $ SERIAL_NUMBER=$(nrfutil device list | grep THINGY)
 $ nrfutil device program --firmware build/dfu_application.zip --serial-number $SERIAL_NUMBER
 ```
 
-## Connect 
+## Connect
 
 ```
 $ picocom -b 115200 /dev/ttyACM0
 Terminal ready
 *** Booting nRF Connect SDK v3.2.1-d8887f6f32df ***
 *** Using Zephyr OS v4.2.99-ec78104f1569 ***
-[00:00:00.291,748] <inf> lte_ping: Initializing modem.
-[00:00:00.627,593] <inf> lte_ping: Connecting to LTE network.
-[00:00:04.616,668] <inf> lte_ping: Connected to LTE network.
+[00:00:00.291,748] <inf> main: Initializing the modem firmware.
+[00:00:00.627,593] <inf> main: Connecting to LTE network.
+[00:00:04.616,668] <inf> main: Connected to LTE network.
+[00:00:05.957,061] <inf> main: MQTT connected.
+[00:00:05.958,374] <inf> main: Published: Hello #0
+[00:00:06.403,045] <inf> main: Subscribed (msg id 1)
+[00:00:15.968,017] <inf> main: Published: Hello #1
+[00:00:25.974,822] <inf> main: Published: Hello #2
+[00:03:58.508,850] <inf> main: Received: foo on topic: thingy91x/command
 ...
 ctrl-a ctrl-x
+```
+
+## Listen to MQTT messages
+```
+$ mosquitto_sub -h broker.emqx.io -t "thingy91x/hello"
+Hello #1
+Hello #2
+...
+```
+
+## Sending a MQTT command to the device
+
+```
+$ mosquitto_pub -h broker.emqx.io -t "thingy91x/command" -m "foo"
 ```
--- 005_lte_mqtt/src/main.c
+++ 005_lte_mqtt/src/main.c
@@ -2,16 +2,58 @@
 #include <zephyr/logging/log.h>
 #include <modem/lte_lc.h>
 #include <modem/nrf_modem_lib.h>
+#include <net/mqtt_helper.h>
 
-LOG_MODULE_REGISTER(lte_mqtt, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+#define MQTT_BROKER_HOSTNAME "broker.emqx.io"
+#define MQTT_CLIENT_ID "thingy91x"
+#define MQTT_PUB_TOPIC "thingy91x/hello"
+#define MQTT_SUB_TOPIC "thingy91x/command"
+
+static K_SEM_DEFINE(mqtt_connected_sem, 0, 1);
+
+static void on_connack(enum mqtt_conn_return_code return_code, bool session_present)
+{
+    if (return_code != MQTT_CONNECTION_ACCEPTED) {
+        LOG_ERR("MQTT connection rejected: %d", return_code);
+        return;
+    }
+    LOG_INF("MQTT connected.");
+    /* Allow main() to continue by releaseing the semaphore. */
+    k_sem_give(&mqtt_connected_sem);
+}
+
+static void on_disconnect(int result)
+{
+    LOG_INF("MQTT disconnected: %d", result);
+}
+
+static void on_publish(struct mqtt_helper_buf topic, struct mqtt_helper_buf payload)
+{
+    LOG_INF("Received: %.*s on topic: %.*s",
+        payload.size, payload.ptr,
+        topic.size, topic.ptr
+    );
+}
+
+static void on_suback(uint16_t message_id, int result)
+{
+    if (result == 0) {
+        LOG_INF("Subscribed (msg id %d)", message_id);
+    } else {
+        LOG_ERR("Subscribe failed: %d", result);
+    }
+}
 
 int main(void)
 {
     int rc;
+
     LOG_INF("Initializing the modem firmware.");
     rc = nrf_modem_lib_init();
     if (rc < 0) {
-        LOG_ERR("Modem init failed: %d", rc);
+        LOG_ERR("NRF model lib init failed: %d", rc);
         return rc;
     }
 
@@ -19,13 +61,90 @@ int main(void)
     LOG_INF("Connecting to LTE network.");
     rc = lte_lc_connect();
     if (rc < 0) {
-        LOG_ERR("LTE connect failed: %d", rc);
+        LOG_ERR("LTE lc connect failed: %d", rc);
         return rc;
     }
     LOG_INF("Connected to LTE network.");
 
+    /* Initialize MQTT helper with callbacks. */
+    struct mqtt_helper_cfg cfg = {
+        .cb = {
+            .on_connack = on_connack,
+            .on_disconnect = on_disconnect,
+            .on_publish = on_publish,
+            .on_suback = on_suback,
+        },
+    };
+
+    rc = mqtt_helper_init(&cfg);
+    if (rc < 0) {
+        LOG_ERR("MQTT helper init failed: %d", rc);
+        return rc;
+    }
+
+    /* Connect to the MQTT broker. */
+    struct mqtt_helper_conn_params conn_params = {
+        .hostname.ptr = MQTT_BROKER_HOSTNAME,
+        .hostname.size = strlen(MQTT_BROKER_HOSTNAME),
+        .device_id.ptr = MQTT_CLIENT_ID,
+        .device_id.size = strlen(MQTT_CLIENT_ID),
+    };
+
+    rc = mqtt_helper_connect(&conn_params);
+    if (rc < 0) {
+        LOG_ERR("MQTT helper connect failed: %d", rc);
+        return rc;
+    }
+
+    /* Wait for CONNACK from broker. */
+    rc = k_sem_take(&mqtt_connected_sem, K_SECONDS(30));
+    if (rc < 0) {
+        LOG_ERR("MQTT connect timed out");
+        return rc;
+    }
+
+    /* Subscribe to a topic. */
+    struct mqtt_topic topics[] = {
+        {
+            .topic.utf8 = MQTT_SUB_TOPIC,
+            .topic.size = strlen(MQTT_SUB_TOPIC),
+        },
+    };
+    struct mqtt_subscription_list sub_list = {
+        .list = topics,
+        .list_count = ARRAY_SIZE(topics),
+        .message_id = mqtt_helper_msg_id_get(),
+    };
+
+    rc = mqtt_helper_subscribe(&sub_list);
+    if (rc < 0) {
+        LOG_ERR("MQTT helper subscribe failed: %d", rc);
+    }
+
+    /* Publish a message every 10 seconds. */
+    char payload[64];
+    int count = 0;
+
     while (1) {
-        k_sleep(K_SECONDS(1));
+        snprintk(payload, sizeof(payload), "Hello #%d", count++);
+
+        struct mqtt_publish_param param = {
+            .message.payload.data = payload,
+            .message.payload.len = strlen(payload),
+            .message.topic.qos = MQTT_QOS_1_AT_LEAST_ONCE,
+            .message_id = mqtt_helper_msg_id_get(),
+            .message.topic.topic.utf8 = MQTT_PUB_TOPIC,
+            .message.topic.topic.size = strlen(MQTT_PUB_TOPIC),
+        };
+
+        rc = mqtt_helper_publish(&param);
+        if (rc < 0) {
+            LOG_ERR("MQTT helper publish failed: %d", rc);
+        } else {
+            LOG_INF("Published: %s", payload);
+        }
+
+        k_sleep(K_SECONDS(10));
     }
     return 0;
 }
