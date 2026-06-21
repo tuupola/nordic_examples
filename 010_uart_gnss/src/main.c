be77c2c12b8780521b275488f47483064c3d25b7
Mika Tuupola <tuupola@appelsiini.net>
2026-06-21 23:42:52 +0300

    Read and parse data after UART_RX_RDY event

--- 010_uart_gnss/src/main.c
+++ 010_uart_gnss/src/main.c
@@ -10,11 +10,13 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 /* DMA buffers for the UART driver */
 #define RX_BUF_SIZE 2048
-#define RX_TIMEOUT_US 100000
+#define RX_TIMEOUT_US 10000
 
 /* Used for decoupling ISR from the main thread */
 #define RING_BUF_SIZE 2048
 RING_BUF_DECLARE(rx_ring, RING_BUF_SIZE);
+
+static K_SEM_DEFINE(rx_sem, 0, 1);
 
 static const struct device *const uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
 
@@ -25,24 +27,36 @@ static uint8_t buf_idx;
 lwgps_t gps;
 
 static void process_gnss(const uint8_t *data, size_t len) {
-    LOG_HEXDUMP_INF(data, len, "GNSS");
-    LOG_INF("GNSS data %d bytes", len);
+    LOG_HEXDUMP_DBG(data, len, "GNSS");
 
     lwgps_process(&gps, data, len);
-    LOG_INF("lat=%.6f lon=%.6f fix=%d sats=%d",
-        (double)gps.latitude, (double)gps.longitude, gps.fix, gps.sats_in_use
+    LOG_INF(
+        "lat=%.6f lon=%.6f fix=%d sats=%d",
+        gps.latitude,
+        gps.longitude,
+        gps.fix,
+        gps.sats_in_use
     );
 }
 
 static void
 uart_callback(const struct device *dev, struct uart_event *event, void *user_data) {
+    uint32_t written;
+
     switch (event->type) {
         case UART_RX_RDY:
             /* Move GNSS parsing out of ISR context */
-            LOG_DBG("UART_RX_RDY");
-            ring_buf_put(
+            written = ring_buf_put(
                 &rx_ring, event->data.rx.buf + event->data.rx.offset, event->data.rx.len
             );
+            /* Warn if ringbuffer is full. */
+            if (written < event->data.rx.len) {
+                LOG_WRN(
+                    "Ringbuffer overflow, dropped %u bytes", event->data.rx.len - written
+                );
+            }
+            /* Release semaphosre so main() can continue. */
+            k_sem_give(&rx_sem);
             break;
 
         case UART_RX_BUF_REQUEST:
@@ -53,7 +67,7 @@ uart_callback(const struct device *dev, struct uart_event *event, void *user_dat
 
         case UART_RX_DISABLED:
             /* Re-enable after timeout or error. */
-            LOG_WRN("RX disabled, re-enabling");
+            LOG_WRN("UART_RX_DISABLED, re-enabling");
             uart_rx_enable(dev, rx_buf[0], RX_BUF_SIZE, RX_TIMEOUT_US);
             buf_idx = 0;
             break;
@@ -63,7 +77,7 @@ uart_callback(const struct device *dev, struct uart_event *event, void *user_dat
             break;
 
         case UART_TX_ABORTED:
-            LOG_WRN("TX aborted");
+            LOG_WRN("UART_TX_ABORTED");
             break;
 
         default:
@@ -99,13 +113,13 @@ int main(void) {
     /* Main loop processes GNSS messages from ring buffer  */
     while (1) {
         static uint8_t tmp[RX_BUF_SIZE];
+        /* Wait for UART_RX_RDY to give semaphore. */
+        k_sem_take(&rx_sem, K_FOREVER);
         uint32_t len = ring_buf_get(&rx_ring, tmp, sizeof(tmp));
-        LOG_INF("ring_buf_get() %d bytes", len);
         if (len > 0) {
+            LOG_INF("ring_buf_get() %u bytes", len);
             process_gnss(tmp, len);
         }
-
-        k_sleep(K_MSEC(100));
     }
     return 0;
 }
